4e33ea569eddb28a02854bee262ea2b3474f3fdc
Lars Schotte <gustik@gustik.eu>
2019-06-01 19:58:29 +0200

    started reworking of resign_dnssec script

--- resign_dnssec.sh
+++ resign_dnssec.sh
@@ -1,31 +1,51 @@
-#!/bin/sh
+#!/bin/sh -x
 
-zonefilesdir="/var/www/zonefiles"
-autodnssecdir="${HOME}/software/autodnssec"
-dnssecfilesdir="${HOME}/content/dnssec"
+# location of zone files (default: /home/{user}/work/zonefiles )
+zonefilesdir="${HOME}/work/zonefiles"
+# this project directory (default: this directory)
+autodnssecdir="$(dirname $0)"
+# location of dnssec keys (default: /home/{user}/work/dnssec)
+dnssecfilesdir="${HOME}/work/dnssec"
 
-for i in "$zonefilesdir" "$autodnssecdir" "$dnssecfilesdir"
+while [ $# -ge 1 ]
 do \
-  if [ ! -d "$i" ]
-  then \
-    echo "Path $i is not a directory, exitting."
-    exit 1
-  fi
+  case "$1" in
+    "-z") shift
+          zonefilesdir="$1" ;;
+    "-a") shift 
+          autodnssecdir="$1" ;;
+    "-d") shift 
+          dnssecfilesdir="$1" ;;
+  esac
+  shift
 done
 
-if cd "$zonefilesdir"
-then \
-  for i in *.zone
+testifdirs() {
+  for i in "$zonefilesdir" "$autodnssecdir" "$dnssecfilesdir"
   do \
-    newfilecontent="$(cat "$i" | gawk -f "$autodnssecdir"/"increment_soa_serial.awk")"
-    if [ -n "$newfilecontent" ]
+    if [ ! -d "$i" ]
     then \
-      echo "$newfilecontent" | diff -w "$i" - | patch --quiet "$i"
-    else \
-      echo "Failed to increment SOA serial, exitting."
+      echo "Path $i is not a directory, exitting."
       exit 1
     fi
   done
-  "${autodnssecdir}/signzonescript.sh" "$dnssecfilesdir"
-  svn --quiet commit -m `date +%F` &&  svn --quiet up && svn diff -r PREV --diff-cmd diff -x -w
-fi
+}
+
+alltherest() {
+  if cd "$zonefilesdir"
+  then \
+    for i in *.zone
+    do \
+      newfilecontent="$(cat "$i" | gawk -f "$autodnssecdir"/"increment_soa_serial.awk")"
+      if [ -n "$newfilecontent" ]
+      then \
+        echo "$newfilecontent" | diff -w "$i" - | patch --quiet "$i"
+      else \
+        echo "Failed to increment SOA serial, exitting."
+        exit 1
+      fi
+    done
+    "${autodnssecdir}/signzonescript.sh" "$dnssecfilesdir"
+    svn --quiet commit -m `date +%F` &&  svn --quiet up && svn diff -r PREV --diff-cmd diff -x -w
+  fi
+}
